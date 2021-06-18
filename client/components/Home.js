451d945e2fb0f942dc57cd4d520ada5554310b2f
Kelsey Siman <ksiman14@gmail.com>
2021-06-18 15:49:51 -0600

    style: landing page

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 
 const Home = () => {
-  return <Box></Box>;
+  return (
+    <Box>
+      <Container maxWidth="sm">
+        <h1>hello world!</h1>
+        <img alt="Doctored Up" src="./logo_whitebg.png" width="100%" />
+        <h1>coming soon...</h1>
+      </Container>
+    </Box>
+  );
 };
 
 export default Home;
