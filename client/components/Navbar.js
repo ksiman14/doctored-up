b7c5c478ef016289959fb9bd9fd1d26246cdf271
Kelsey Siman <ksiman14@gmail.com>
2021-06-18 19:24:58 -0600

    feat: Navbar with title

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 const Navbar = () => {
-  return <nav></nav>;
+  return (
+    <AppBar style={{ background: '#90caf9' }}>
+      <Toolbar>
+        <h2>doctored up</h2>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
