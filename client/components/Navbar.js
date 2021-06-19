d46f8615f0ad323049626c380cd4de44b48da2d9
Kelsey Siman <ksiman14@gmail.com>
2021-06-18 19:32:07 -0600

    style: elevated Navbar on scroll

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
-const Navbar = () => {
+const ElevationScroll = (props) => {
+  const { children, window } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+};
+
+const Navbar = (props) => {
   return (
-    <AppBar style={{ background: '#90caf9' }}>
-      <Toolbar>
-        <h2>doctored up</h2>
-      </Toolbar>
-    </AppBar>
+    <Fragment>
+      <ElevationScroll {...props}>
+        <AppBar style={{ background: '#90caf9' }}>
+          <Toolbar>
+            <h2>doctored up</h2>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
+    </Fragment>
   );
 };
 
